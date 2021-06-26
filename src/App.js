dbe7b047ca789e984fe625a3d17159908bd353df
Onuh Chukwuma. M <onuhchukwuma12@gmail.com>
2021-06-27 00:44:42 +0100

    deploying the app

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component{
       );
     return ( 
       <div className = 'App'>  
+        <h1> Monsters Roledex </h1>
         <SearchBox
           placeholder='Search monsters'
           handleChange={this.handleChange}
