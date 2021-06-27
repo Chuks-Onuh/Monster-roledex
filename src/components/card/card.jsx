50e13d1af160efd0c47a7ffe2fa78d92931bd9d3
Onuh Chukwuma. M <onuhchukwuma12@gmail.com>
2021-06-27 01:36:39 +0100

    fixed bug

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,7 +4,9 @@ import './card.css';
 
 export const Card = (props) => (
     <div className='card-container'>
+        <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set1$size=10x10`} />
         <h3>{props.monster.name}</h3>
+        <p>{props.monster.email}</p>
     </div>
 )
 
